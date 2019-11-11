f121b51f793ad35817fd7368ef72226363498f30
lau-hubler <lauanahubler@gmail.com>
2019-11-10 22:11:40 -0500

    change name into a variable

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-
+$var1 = 1;
+$name = 'Hector Benitez';
 ?>
 
 <!doctype html>
@@ -25,7 +26,7 @@
         <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
       </div>
       <div class="col">
-        <h1><?php echo 'Hector Benitez'; ?></h1>
+        <h1><?php echo $name; ?></h1>
         <h2>PHP Developer</h2>
         <ul>
           <li>Mail: hector@mail.com</li>
